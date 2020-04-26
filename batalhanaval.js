d8d70f550d32d14ed42e81e6b033bec1f56c942c
Marina Maiochi <marina.maiochi@gmail.com>
2020-04-26 13:21:25 +0200

    pc jogando inteligente mas com coisas a melhorar

--- batalhanaval.js
+++ batalhanaval.js
@@ -436,21 +436,88 @@ function jogadaPc(){
     if (estadoJogo !== estados.PC){
         return;
     } 
+    const ultimosAcertos = [];
     let ultimoAcertoPc;
-    for (let i = jogadasPc.length -1; i >= 0; i--) {
+    for (let i = 0; i < jogadasPc.length; i++) {
         if (jogadasPc[i].classList.contains('bomba')){
             ultimoAcertoPc = jogadasPc[i];
-            break;
+            ultimosAcertos.push(ultimoAcertoPc);
         }
     }
     let celulaPraJogar;
     if (!ultimoAcertoPc || verificaUltimoNavioBomb(ultimoAcertoPc)){
+        console.info('nao acertei nada ou ultimo navio bomb jogando aleatorio');
         celulaPraJogar = escolheCelulaAleat();
-        console.info('nao acertei nada ou ultimo navio bomb');
     } else {
         // teve ultimo acerto e nao destruiu o navio
-        celulaPraJogar = adjacenteAleat(ultimoAcertoPc);
-        console.info('teve ultimo acerto e nao destruiu o navio');
+        console.info('teve ultimo acerto e nao destruiu o navio , escolhendo direcao para jogar');
+        if (ultimosAcertos.length == 1){
+            console.info('só acertei 1, procurando adjacente');
+            celulaPraJogar = adjacenteAleat(ultimoAcertoPc);
+        } else {
+            console.info('teve + de 1 acerto');
+            const celUltAcerto = ultimosAcertos[ultimosAcertos.length-1]
+            const celPenultAcerto = ultimosAcertos[ultimosAcertos.length-2]
+
+            const ultN = celUltAcerto.getAttribute('data-n');
+            const penultN = celPenultAcerto.getAttribute('data-n');
+            if (ultN != penultN){
+                console.info('teve + de 1 acertonavios diferentes, jogando adjacente do ultimo');
+                celulaPraJogar = adjacenteAleat(ultimoAcertoPc);
+            } else {
+                console.info('navios iguais');
+                const colultN = parseInt(celUltAcerto.getAttribute('data-coluna'));
+                const colpenultN = parseInt(celPenultAcerto.getAttribute('data-coluna'));
+                const linultN = parseInt(celUltAcerto.getAttribute('data-linha'));
+                const linpenultN = parseInt(celPenultAcerto.getAttribute('data-linha'));
+                const difLinha = linultN - linpenultN;   
+                const difColuna = colultN - colpenultN;  
+                console.info(`diflinha:${difLinha} difcoluna${difColuna}`);
+                if(colultN == colpenultN){
+                    if (linultN < 9 && linultN > 0) {
+                        console.info('nao esta nas bordas da linha, jogando na segqieuncia');
+                        celulaPraJogar = pegaCelTabJog(linultN+difLinha,colultN);
+                        if(celulaPraJogar.classList.contains('bombaagua') || celulaPraJogar.classList.contains('bomba')){
+                            console.info('seq de jogada ja tem bomba, jogandoparaouyro lado');
+                            const primeiroAcertoNavio = ultimosAcertos.find(function(acerto){
+                                return acerto.getAttribute('data-n') == ultN;
+                            })
+                            const linPriAcertoNavio = parseInt(primeiroAcertoNavio.getAttribute('data-linha'));
+                            celulaPraJogar = pegaCelTabJog(linPriAcertoNavio-difLinha,colultN);
+                        }
+                    } else {
+                        console.info('estou na borda da linha, jogando pro oiutro lado');
+                        const primeiroAcertoNavio = ultimosAcertos.find(function(acerto){
+                            return acerto.getAttribute('data-n') == ultN;
+                        })
+                        const linPriAcertoNavio = parseInt(primeiroAcertoNavio.getAttribute('data-linha'));
+                        celulaPraJogar = pegaCelTabJog(linPriAcertoNavio-difLinha,colultN);
+                    }
+                    
+                } else {
+                    if (colultN < 9 && colultN > 0) {
+                        console.info('nao esta nas bordas da coluna, jogando na segqieuncia');
+                        celulaPraJogar = pegaCelTabJog(linultN,colultN+difColuna);
+                        if(celulaPraJogar.classList.contains('bombaagua') || celulaPraJogar.classList.contains('bomba')){
+                            console.info('seq de jogada ja tem bomba, jogandoparaouyro lado');
+                            const primeiroAcertoNavio = ultimosAcertos.find(function(acerto){
+                                return acerto.getAttribute('data-n') == ultN;
+                            })
+                            const colPriAcertoNavio = parseInt(primeiroAcertoNavio.getAttribute('data-coluna'));
+                            celulaPraJogar = pegaCelTabJog(linultN,colPriAcertoNavio-difColuna);
+                        }
+                    } else {
+                        console.info('estou na borda da coluna, jogando pro oiutro lado');
+                        const primeiroAcertoNavio = ultimosAcertos.find(function(acerto){
+                            return acerto.getAttribute('data-n') == ultN;
+                        })
+                        const colPriAcertoNavio = parseInt(primeiroAcertoNavio.getAttribute('data-coluna'));
+                        celulaPraJogar = pegaCelTabJog(linultN,colPriAcertoNavio-difColuna);
+                    }
+                    
+                }
+            }
+        }    
     }
     acertoPc += atiraNaCelula(celulaPraJogar);
     jogadasPc.push(celulaPraJogar);
@@ -459,6 +526,9 @@ function jogadaPc(){
     setTimeout(function() {
         trocaEstado(estados.JOGADOR)
     },1000);
+}
+function pegaCelTabJog(linha,coluna){
+    return document.querySelector(`.tabuleiroJog [data-coluna="${coluna}"][data-linha="${linha}"]`)
 }
 function escolheCelulaAleat(){
     const jogaveis = document.querySelectorAll(`.tabuleiroJog .celula:not(.bomba):not(.bombaagua)`);
@@ -476,25 +546,25 @@ function adjacenteAleat(celula){
     const lin = parseInt(celula.getAttribute('data-linha'),10); 
     const adjPossiveis = [];
     if(col > 0){
-        const celOeste = document.querySelector(`[data-coluna="${col-1}"][data-linha="${lin}"]`);
+        const celOeste = pegaCelTabJog(lin,col-1);
         if (!celOeste.classList.contains('bomba') && !celOeste.classList.contains('bombaagua')){
             adjPossiveis.push(celOeste);
         }
     }
     if(col < 9){
-        const celLeste = document.querySelector(`[data-coluna="${col+1}"][data-linha="${lin}"]`);
+        const celLeste = pegaCelTabJog(lin,col+1);
         if (!celLeste.classList.contains('bomba') && !celLeste.classList.contains('bombaagua')){
             adjPossiveis.push(celLeste);
         }
     }
     if(lin > 0){
-        const celNorte = document.querySelector(`[data-coluna="${col}"][data-linha="${lin-1}"]`);
+        const celNorte = pegaCelTabJog(lin-1,col);
         if (!celNorte.classList.contains('bomba') && !celNorte.classList.contains('bombaagua')){
             adjPossiveis.push(celNorte);
         }
     }
     if(lin < 9){
-        const celSul = document.querySelector(`[data-coluna="${col}"][data-linha="${lin+1}"]`);
+        const celSul = pegaCelTabJog(lin+1,col);
         if (!celSul.classList.contains('bomba') && !celSul.classList.contains('bombaagua')){
             adjPossiveis.push(celSul);
         }
